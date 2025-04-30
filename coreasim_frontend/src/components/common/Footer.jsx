ade27e862cc6f24d77239640ad9a72eb360747a5
Juben George John <jubenjohn25@gmail.com>
2025-04-30 16:36:48 +0530

    Added navbar and footer

---
+++ coreasim_frontend/src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-100 py-4 mt-20">
+        <div className="text-center text-sm text-gray-600">
+          © {new Date().getFullYear()} CoreaSim. All rights reserved.
+        </div>
+      </footer>
+    );
+  }
+  
